914ae7bfee7b51e4311ac73a192a3c0d56d774f7
abdaluh172-del <abdaluh172@gmail.com>
2026-07-15 20:40:15 +0300

    Load model and initialize database on startup

--- app.py
+++ app.py
@@ -431,6 +431,8 @@ def dashboard_stats():
     })
 
 
+MODEL = load_model()
+init_db()
+
 if __name__ == "__main__":
-    init_db()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
